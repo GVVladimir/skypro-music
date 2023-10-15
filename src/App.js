291c15ba413c17fa91add604a2736d1a432c97a3
GVVladimir <115398661+GVVladimir@users.noreply.github.com>
2023-10-15 15:06:01 +0300

    skymusdz

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p class="title-text">
+        <p>
         Здесь скоро будет Skypro.Music.
         </p>
         <a
